cee74bc51df88f5e5a0cb141a71b0db0ea6dd4df
Omkar Wani <omkar.wani55@gmail.com>
2023-05-15 22:12:41 +0530

    New Commit

---
+++ TripletSumZero.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class TripletSumZero{
+    public static void findTriplet(int[] arr,int n){
+        boolean found = false;
+        for(int i=0;i<n-1;i++){
+        Set<Integer> set = new HashSet<>();
+        for(int j=i+1;j<n;j++){
+            int x = -(arr[i]+arr[j]);
+            if(set.contains(x)){
+                System.out.println(x+ " " +arr[i]+" "+arr[j]);
+                found = true;
+            }else{
+                set.add(arr[j]);
+            }
+        }
+        }
+        if(!found){
+            System.out.println("no triplet found");
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        findTriplet(arr,n);
+    }
+}
