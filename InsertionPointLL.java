91be9b1b2e593d309c7e8184e7ced8d98d66b3c7
Omkar Wani <omkar.wani55@gmail.com>
2023-05-16 23:51:25 +0530

    new commit

---
+++ InsertionPointLL.java
@@ -0,0 +1,107 @@
+// Q. given two linked lists, find point where the two linked lists intersect each other
+
+import java.util.Scanner;
+// lINK LIS
+//1- head,2- data part(value), 3- Link part(link of the next node), 4- last node should be null(end of the node)
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+ 
+ 
+public class InsertionPointLL{
+ 
+    public static void main(String [] args){
+        // get the value from the user 
+        Scanner sc = new Scanner(System.in);
+         // gert the first linked IllegalMonitorStateException
+         Node head1 = creeateLinkedList(sc);
+            Node head2 = creeateLinkedList(sc);
+                Node common = creeateLinkedList(sc);
+ 
+
+    //connecting the common part
+ 
+    Node current = head1;
+    while(current.next != null){
+        current=current.next;
+    }
+    current.next = common;
+ 
+    current = head2;
+    while(current.next != null){
+        current = current.next;
+    }
+    current.next = common;
+ 
+    // find the insertion
+    Node insertionNode = getInsertionNode(head1, head2);
+    if(insertionNode != null){
+        System.out.println("Insertion point: "+insertionNode.data);
+    }else{
+        System.out.println("No insertion point found ");
+    }
+}
+ 
+ 
+    // creating the LL
+ 
+    private static Node creeateLinkedList(Scanner sc){
+    Node dummy = new Node(0);
+    Node current = dummy;
+ 
+    int data;
+    while(true){
+        data = sc.nextInt();
+        if(data == -1){
+            break;
+        }
+
+    }
+    return dummy.next;
+    }
+    public static Node getInsertionNode(Node head1, Node head2){
+        int len1 = getLinkedListLength(head1);
+        int len2 = getLinkedListLength(head2);
+ 
+        int diff = Math.abs(len1 - len2);
+ 
+        Node curr1 = head1;
+        Node curr2 = head2;
+ 
+        // Moving longer list
+ 
+        if(len1 > len2){
+            for(int i =0; i<diff; i++){
+                curr1 = curr1.next;
+            }
+          }  else{
+                for(int i=0; i< diff; i++){
+                    curr2 = curr2.next;
+                }
+            }
+    while (curr1 != null && curr2 != null){
+        if(curr1==curr2){
+            return curr1;
+        }
+        curr1 = curr1.next;
+        curr2 = curr2.next;
+        }
+        return null; // no insertion poing 
+    }
+ 
+    private static int getLinkedListLength(Node head){
+        int length =0;
+        Node current = head;
+        while(current != null){
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+}
+
