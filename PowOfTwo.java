880541412658becf25553775ef26249605f2d733
Omkar Wani <omkar.wani55@gmail.com>
2023-05-18 00:04:46 +0530

    new commit

---
+++ PowOfTwo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Scanner;
+
+class PowOfTwo {
+    public static boolean isPowerOfTwo(int n) {
+        if(n<=0)return false;
+
+        if(n==1)return true;
+
+        if(n % 2 != 0 ) return false;
+
+        return isPowerOfTwo(n/2);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        isPowerOfTwo(n);
+    }
+}
