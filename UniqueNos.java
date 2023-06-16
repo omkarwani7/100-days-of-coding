8f2fec42859ccbbd9afb4bfac37d15bb43a71dcd
Omkar Wani <omkar.wani55@gmail.com>
2023-06-16 18:26:45 +0530

    New Commit

---
+++ UniqueNos.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class UniqueNos{
+    public static void main(String[] args){
+        int[] arr = {10,11,12,13,14,15};
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = arr[i] % 10;
+            int last = arr[i];
+            int first = arr[i] / 10;
+            if( first != last){
+                count++;
+            }
+
+        }
+        System.out.println(count);
+        
+    }
+}
