8303dfb1f2a1e6207fb19a78267409061e726b5e
Omkar Wani <omkar.wani55@gmail.com>
2023-06-20 21:57:08 +0530

    New Commit

--- PairContainers.java
+++ PairContainers.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class PairContainers{
     public static void main(String[] args){
-        int arr[] = {100,560,23,53,19,20};
+        int arr[] = {100,560,23,19,20};
         int n = arr.length;
         int j = n-1;
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -11,8 +11,17 @@ class PairContainers{
         //         map.put(arr[i], arr[j]);
         //     }
         // }
+        // for(int i=0;i<n/2;i++){
+        //     map.put(arr[i], arr[j--]);
+        // }
+        // System.out.println(map);
         for(int i=0;i<n/2;i++){
+            if(n % 2 == 0){
             map.put(arr[i], arr[j--]);
+          }else{
+            map.put(arr[i], arr[j--]);
+            map.put(arr[n/2], 0);
+          }
         }
         System.out.println(map);
     }
