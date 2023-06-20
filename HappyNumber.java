8978e5397e5330b7df7554481a676eb85836a9ac
Omkar Wani <omkar.wani55@gmail.com>
2023-06-20 22:30:25 +0530

    New Commit

---
+++ HappyNumber.java
@@ -0,0 +1,23 @@
+// leetcode Q. 202
+
+class HappyNumber {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n != 1 && !set.contains(n)){
+            set.add(n);
+            n = getNextNum(n);
+        }
+        return n==1;
+    }    
+
+        public int getNextNum(int n){
+            int totalsum = 0;
+            while(n>0){
+                int temp = n % 10;
+                n = n / 10;
+                totalsum += temp * temp;
+            }
+            return totalsum;
+        
+    }
+}
