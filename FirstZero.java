1caf1ec5d5964cb55375ababd350185df63a79b4
Omkar Wani <omkar.wani55@gmail.com>
2023-07-07 14:03:45 +0530

    new commit

---
+++ FirstZero.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FirstZero{
+    public static void main(String[] args){
+        int[] arr = {1,0,1,0,0,0,1};
+        int current = arr.length - 1;
+        for(int i=0;i>=arr.length-1;i--){
+            if(arr[i]!=0){
+                arr[current] = arr[i];
+                current--;
+            }
+            while(current>=0){
+                arr[current] = 0;
+                current--;
+            }
+            for(i=0;i<arr.length;i++){
+                System.out.println(arr[i] + " ");
+            }
+        }
+    }
+}
