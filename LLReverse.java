25bce76e13a5cd15f8379ee2c2506b460e47621d
Omkar Wani <omkar.wani55@gmail.com>
2023-07-10 23:17:50 +0530

    new commit

---
+++ LLReverse.java
@@ -0,0 +1,29 @@
+// leetcode Q. 206
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class LLReverse {
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
