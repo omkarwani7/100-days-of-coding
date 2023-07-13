e6a47ba73e94a1550c7314e0d14e0c989d0b1d8b
Omkar Wani <omkar.wani55@gmail.com>
2023-07-13 22:35:12 +0530

    new commit

---
+++ SubarrayKSimilarElem.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SubarrayKSimilarElem{
+    public static void main(String[] args){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        // arr = [4,3,4,3]
+
+
+    }
+}
