6d2be8683ef696d4fa7767250c2c02113f911582
Omkar Wani <omkar.wani55@gmail.com>
2023-08-03 11:40:28 +0530

    New Commit

--- IntToRoman.java
+++ IntToRoman.java
@@ -1,7 +1,8 @@
 // leetcode Q. 12
 
+import java.util.*;
 class IntToRoman {
-    public String intToRoman(int num) {
+    public static String intToRoman(int num) {
         int[] intNums = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder sb = new StringBuilder();
@@ -11,5 +12,10 @@ class IntToRoman {
                 num-=intNums[i];
             }
         }return sb.toString();
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=84;
+        System.out.println(intToRoman(num));
     }
 }
