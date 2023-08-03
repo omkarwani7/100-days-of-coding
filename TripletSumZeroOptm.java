fc4466fd8f4ab0c5aa75fd51c569a9af40d75687
Omkar Wani <omkar.wani55@gmail.com>
2023-08-03 19:09:07 +0530

    New Commit

--- TripletSumZeroOptm.java
+++ TripletSumZeroOptm.java
@@ -1,19 +1,21 @@
 import java.util.*;
 public class TripletSumZeroOptm{
     public static void main(String[] args){
-        int[] arr={0,-1,2,-3,1};
+        //0,-1,2,-3,1
+        int[] arr={7,3,0,10,0};;
         int n=arr.length;
-        findTriplets(arr,n);
+        int target=10;
+        findTriplets(arr,n,target);
     }
-    public static void findTriplets(int[] arr,int n){
+    public static void findTriplets(int[] arr,int n,int target){
         boolean found=false;
         Arrays.sort(arr);
         for(int i=0;i<n-1;i++){
             int l=i+1;
             int r=n-1;
             int x=arr[i];
-            while(l<r){
-                if(arr[i]+arr[l]+arr[r]==0){
+            while(l<r){  
+                if(arr[i]+arr[l]+arr[r]==target){
                     System.out.print(x+" ");
                     System.out.print(arr[l]+" ");
                     System.out.println(arr[r]);
