621c734b3a7b0ccec5559aee9a292ea00dd538b5
Omkar Wani <omkar.wani55@gmail.com>
2023-08-12 11:48:32 +0530

    new commit

---
+++ LargeSmallSum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class LargeSmallSum{
+    public static int getSum(int[] arr,int n){
+        int[] even=new int[n/2+1];
+        int[] odd=new int[n/2+1];
+        int a=0,b=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                even[a]=arr[i];
+                a++;
+            }else{
+                odd[b]=arr[i];
+                b++;
+            }
+        }
+        Arrays.sort(even);
+        Arrays.sort(odd);
+        int x=even.length;
+        int y=odd.length;
+        int evenelem=even[x-2];
+        int oddelem=odd[2];
+        return evenelem+oddelem;
+    }
+    public static void main(String[] args){
+        int[] arr={3,2,1,7,5,4};
+        int n=arr.length;
+        System.out.println(getSum(arr,n));
+    }
+}
