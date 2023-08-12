4893c46d6c22d49172a0166d01d3a2d8b919f493
Omkar Wani <omkar.wani55@gmail.com>
2023-08-12 11:49:50 +0530

    new commit

--- LargeSmallSum.java
+++ LargeSmallSum.java
@@ -1,3 +1,8 @@
+// The function accepts an integers arr of size ’length’ as its arguments you are required to 
+// return the sum of second largest  element from the even positions and second smallest from 
+// the odd position of given ‘arr’.
+
+
 import java.util.*;
 public class LargeSmallSum{
     public static int getSum(int[] arr,int n){
